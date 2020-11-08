9019ed9b9d1dcc20fde7279e66ce10553d196f11
itZSaint099 <soumyajithalder099@gmail.com>
2020-11-09 02:12:43 +0530

    Added Bike Controller Added Camera Controller Added Orange Material Changed Purple Material Texture
    - Need to implement deceleration
    - Need to implement control for deceleration
    - Implement reverse rotation

---
+++ Rider/Assets/BikeController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class BikeController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 20f;
+    public float rotationSpeed = 4f;
+
+    bool move = false;
+    bool isGrounded = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            move = true;
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            move = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (move == true)
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+            }
+            else
+            {
+                rb.AddTorque(rotationSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D()
+    {
+        isGrounded = false;
+    }
+}
---
+++ Rider/Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform bike;
+
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = bike.position + offset;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+    }
+}
